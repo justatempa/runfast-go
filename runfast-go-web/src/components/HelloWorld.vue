c2262fb334ea13e1e7ce99639f070b61d05859a4
justatempa <3238785005@qq.com>
2025-10-15 18:08:08 +0800

    feat(project): vue3 前端

---
+++ runfast-go-web/src/components/HelloWorld.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+defineProps<{
+  msg: string
+}>()
+</script>
+
+<template>
+    <h3>
+      You’ve successfully created a project
+
+      {{msg}}
+    </h3>
+</template>
+
+<style scoped>
+h1 {
+  font-weight: 500;
+  font-size: 2.6rem;
+  position: relative;
+  top: -10px;
+}
+
+</style>
---
+++ runfast-go-web/src/views/HomeView.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <h1>This is an home page</h1>
+</template>
